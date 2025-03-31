98a1874ee7a28daceeb9bcde2f6d13e535bf72bd
divanstrawberry <diana.yocker@gmail.com>
2025-03-31 10:28:14 +0300

    онет зачёт
    я надеюсь это всё

---
+++ src/exam_ignateva_231/Main.java
@@ -0,0 +1,103 @@
+package exam_ignateva_231;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        DataBase db = new DataBase();
+        db.isConnection();
+        db.createTableSeans("seans");
+
+        db.addValuesSeans("seans", 1, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 2, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 3, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 4, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 5, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 6, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 7, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 8, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 9, "Алиса в Пограничье", "забронировано");
+        db.addValuesSeans("seans", 10, "Алиса в Пограничье", "свободно");
+
+        db.addValuesSeans("seans", 11, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 12, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 13, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 14, "Алиса в Пограничье", "забронировано");
+        db.addValuesSeans("seans", 15, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 16, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 17, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 18, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 19, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 20, "Алиса в Пограничье", "свободно");
+
+        db.addValuesSeans("seans", 21, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 22, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 23, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 24, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 25, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 26, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 27, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 28, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 29, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 30, "Алиса в Пограничье", "свободно");
+
+        db.addValuesSeans("seans", 31, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 32, "Алиса в Пограничье", "забронировано");
+        db.addValuesSeans("seans", 33, "Алиса в Пограничье", "забронировано");
+        db.addValuesSeans("seans", 34, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 35, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 36, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 37, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 38, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 39, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 40, "Алиса в Пограничье", "свободно");
+
+        db.addValuesSeans("seans", 41, "Алиса в Пограничье", "забронировано");
+        db.addValuesSeans("seans", 42, "Алиса в Пограничье", "забронировано");
+        db.addValuesSeans("seans", 43, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 44, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 45, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 46, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 47, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 48, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 49, "Алиса в Пограничье", "забронировано");
+        db.addValuesSeans("seans", 50, "Алиса в Пограничье", "свободно");
+
+        db.addValuesSeans("seans", 51, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 52, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 53, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 54, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 55, "Алиса в Пограничье", "забронировано");
+        db.addValuesSeans("seans", 56, "Алиса в Пограничье", "забронировано");
+        db.addValuesSeans("seans", 57, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 58, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 59, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 60, "Алиса в Пограничье", "свободно");
+
+        db.addValuesSeans("seans", 61, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 62, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 63, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 64, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 65, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 66, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 67, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 68, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 69, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 70, "Алиса в Пограничье", "свободно");
+
+        db.addValuesSeans("seans", 71, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 72, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 73, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 74, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 75, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 76, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 77, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 78, "Алиса в Пограничье", "свободно");
+        db.addValuesSeans("seans", 79, "Алиса в Пограничье", "забронировано");
+        db.addValuesSeans("seans", 80, "Алиса в Пограничье", "свободно");
+
+        db.selectTableSeans("seans");
+        System.out.println("-------------");
+        db.searchTableSeans("seans");
+    }
+}
